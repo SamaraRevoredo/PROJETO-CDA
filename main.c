923944fefb74a431ee62ab68815287bac69d7e7e
Kaike Castro <42541528+kaikecc@users.noreply.github.com>
2021-03-26 17:40:09 -0300

    arquivo principal

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
+/*
+ * SDR.c
+ *
+ * Created: 26/03/2021 17:20:46
+ * Author : kaike
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 16000000
 
 
-int main(void){
-
-    while(1){
-        
+int main(void)
+{
+	DDRB = (1<<DDRB); // DESTINADO AO DISPLAY LCD
+	
+	DDRD = (1<DDRD); // DESTINA A REDE R2R
+	
+	DDRC = DDRC | B000000011; // PC0 = MSG RECEBIDA, PC1 = LEITURA POT, PC2 = R, PC3 = M, PC4 = P
+	PORTC = B00011100; // DEFINE OS BOTÕES PULLUP
+	
+	
+    /* Replace with your application code */
+    while (1) 
+    {
     }
 }
+
