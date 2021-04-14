3f2235827a5d1b3ddc6b01dd2502545e94f0b420
mohamadsadeque <30416594+mohamadsadeque@users.noreply.github.com>
2021-04-14 16:51:04 -0300

    Criando Pasta individual

---
+++ Mohamad/TesteCentralAlarme/TesteCentralAlarme/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 4/13/2021 12:16:38 AM
+ *  Author: moham
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
