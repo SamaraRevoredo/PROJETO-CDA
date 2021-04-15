7c747a932e93d32cddbd4be0f61b522c575ae00c
mohamad <mohamad.ali.113@ufrn.edu.br>
2021-04-15 01:23:56 -0300

    Driver i2c + Teste com sensores + Arq. proteus

---
+++ Mohamad/TesteCentralAlarme/TesteCentralAlarme/Debug/main.d
@@ -0,0 +1,40 @@
+main.d main.o: .././main.c \
+ c:\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\xc.h \
+ c:\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\cci.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\avr\io.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\avr\sfr_defs.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\inttypes.h \
+ c:\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\stdint.h \
+ C:/Atmel/Studio/7.0/Packs/atmel/ATmega_DFP/1.6.364/xc8/avr/include/avr/iom328p.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\avr\portpins.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\avr\common.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\avr\version.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\avr\fuse.h \
+ c:\microchip\xc8\v2.31\avr\avr\include\avr\lock.h
+
+c:\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\xc.h:
+
+c:\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\cci.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\avr\io.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\avr\sfr_defs.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\inttypes.h:
+
+c:\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\stdint.h:
+
+C:/Atmel/Studio/7.0/Packs/atmel/ATmega_DFP/1.6.364/xc8/avr/include/avr/iom328p.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\avr\portpins.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\avr\common.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\avr\version.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\avr\fuse.h:
+
+c:\microchip\xc8\v2.31\avr\avr\include\avr\lock.h:
--- Mohamad/TesteCentralAlarme/TesteCentralAlarme/main.c
+++ Mohamad/TesteCentralAlarme/TesteCentralAlarme/main.c
@@ -1,16 +1,81 @@
 /*
- * main.c
- *
- * Created: 4/13/2021 12:16:38 AM
- *  Author: moham
+Autor: Mohamad Sadeque
+Driver i2c
  */ 
 
 #include <xc.h>
+typedef enum { escrita, leitura } mode_i2c;
+typedef enum { false, true } bool;
+
+void setupI2C();
+void ERROR();
+bool MT_transmit(uint8_t addr, uint8_t data);
+bool MT_receive(uint8_t addr);
+#define START 0x08
+#define MT_SLA_ACK 0x18
+#define MR_SLA_ACK 0x40
+#define MT_DATA_ACK 0x28
+#define MR_DATA_ACK 0x50
+#define MR_DATA_NACK 0x58
 
 int main(void)
 {
+	setupI2C();
+	PORTB = 0x00;
+	PORTC |= (1<<PORTC3);
+	if(!MT_transmit(0x40,0xff)) PORTB = 0xFF;
+	if(!MT_receive(0x41)) PORTB = 0xAF;
+
+
+
     while(1)
     {
         //TODO:: Please write your application code 
-    }
+	if(!(PINC & (1<<PINC3))) MT_receive(0x41);
+	}
 }
+
+void setupI2C(){
+	DDRC = 0x30; 
+	TWBR = (1<<TWBR1); // Configurando fator de divisão da taxa de bits para 2 
+	TWSR |= (1<<TWPS1); // Configurando Prescaler da taxa  de bits para 16
+}
+
+
+
+
+bool MT_transmit(uint8_t addr, uint8_t data){
+	TWCR = (1<< TWINT)|(1<<TWSTA)|(1<<TWEN); // start
+	while (!(TWCR & (1<<TWINT)));
+	if ((TWSR & 0xF8) != START) return false; 
+	TWDR  = addr;				// envia endereço no modo de escrita
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+	if ((TWSR & 0xF8) != MT_SLA_ACK) return false; 
+	TWDR  = data;				// envia dados
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+	if ((TWSR & 0xF8)!= MT_DATA_ACK) return false;
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO); // stop
+	return true;
+}
+
+bool MT_receive(uint8_t addr){
+	TWCR = (1<< TWINT)|(1<<TWSTA)|(1<<TWEN); // start
+	
+	while (!(TWCR & (1<<TWINT)));
+	if ((TWSR & 0xF8) != START) return false;
+	TWDR  = addr;				// envia endereço no modo de leitura
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+	if ((TWSR & 0xF8) != MR_SLA_ACK) return false;	// confere ACK do escravo após endereçamento
+	
+	TWCR = (1<<TWINT) | (1<<TWEN);					// Recebe byte de dados
+	while (!(TWCR & (1<<TWINT)));
+	if ((TWSR & 0xF8) != MR_DATA_NACK) return false;
+	
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO); // stop
+	PORTD = TWDR;
+	return true;
+}
+
